4eda521bd5225a522c77383f6d35a707e7cd8d17
Troels Agergaard <TroelsAgergaard@users.noreply.github.com>
2023-01-09 10:33:27 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector("body").addEventListener("click", function () {
-    document.querySelector("body").classList.add("animate__tada");
-    setTimeout(() => {
-      document.querySelector("body").classList.remove("animate__tada");
-    }, 500);
-  });
+let articleElement = document.querySelector("article");
+articleElement.addEventListener("click", function () {
+  articleElement.classList.remove("animate__backInUp");
+  articleElement.classList.add("animate__tada");
+  setTimeout(function () {
+    articleElement.classList.remove("animate__tada");
+  }, 500);
 });
+
+let kontaktLink = document.querySelector(".kontakt");
+kontaktLink.addEventListener("click", function (e) {
+  e.preventDefault();
+  articleElement.classList.add("animate__backOutLeft");
+  setTimeout(function () {
+    window.location.assign("kontakt.html");
+  }, 300);
+});
+
+function getAge() {
+  "use strict";
+  age = 21;
+  console.log(age);
+}
+getAge();
